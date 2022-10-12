391c4cd512ccd4348f5a45ca6fd85f35ca790cf8
0nd3 <onni.loponen@gmail.com>
2022-10-12 23:48:25 +0300

    särki mutta pusken silti

---
+++ src/components/ContactUs.js
@@ -0,0 +1,9 @@
+
+
+export default function ContactUs(){
+    return(
+        <div>
+            this is contact us Page
+        </div>
+    )
+}
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+
+
+export default function NotFound(){
+    return(
+        <div>
+            this is not found Page
+        </div>
+    );
+}
